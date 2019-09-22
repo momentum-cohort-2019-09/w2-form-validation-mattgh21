b3c641e2eb7336199dfdcb0aded96ad97ac9b21a
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-09-22 19:20:14 -0400

    adds validation for credit card, step 5

--- script.js
+++ script.js
@@ -35,6 +35,29 @@ function isDateInFuture(date) {
     }
 }
 
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+}
+
 
 
 function main() {
@@ -113,7 +136,7 @@ function main() {
         let ccText = ccInput.value
         console.log({ ccText })
 
-        if (!ccText) {
+        if (!validateCardNumber(ccText)) {
             markInvalid(ccTextField, 'Credit Car is required')
         } else {
             markValid(ccTextField)
